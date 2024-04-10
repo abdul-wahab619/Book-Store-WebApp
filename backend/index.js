94fac5deebe9b50e9916fa887f6a16886e7c6128
abdul-wahab619 <awminhas619@gmail.com>
2024-04-10 19:35:07 +0500

    setting up cors origin

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,33 @@
 import express from "express";
+import cors from "cors";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import bookRoute from "./routes/bookRoute.js";
+
 const app = express();
 
 // middlewares
 app.use(express.json());
 
+// middleware for handling CORS
+
+//option 1: Allow all Origins (*)
+// app.use(cors());
+
+//option 2: Alow Custom Origins
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["content-type"],
+  })
+);
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(619).send("Welcome Abdul Wahab Sab!");
 });
+
+app.cors;
 // Routes
 app.use("/books", bookRoute);
 
